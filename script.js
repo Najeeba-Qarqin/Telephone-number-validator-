b0558ff0b33b13633a3e26f6ea10c16cc64ee4e4
Najeeba <najeebaqarqin@gmail.com>
2024-07-04 06:27:39 +0430

    Solved linter error from stylint and ESlint

--- script.js
+++ script.js
@@ -8,14 +8,6 @@ userInput.addEventListener('keydown', (e) => {
     checkBtn.click();
   }
 });
-
-checkBtn.addEventListener('click', () => {
-  if (userInput.value == '') {
-    alert('PLease provide a phone number');
-    results.innerText = '';
-  } numbervalid();
-});
-
 const numbervalid = () => {
   const validnumbers = [
     /^\d{3}-\d{3}-\d{4}$/,
@@ -25,23 +17,30 @@ const numbervalid = () => {
     /^1 \(\d{3}\) \d{3}-\d{4}$/,
     /^\d{10}$/,
     /^\(\d{3}\)\d{3}-\d{4}$/,
-    /^1\(\d{3}\)\d{3}-\d{4}$/
-  ]
+    /^1\(\d{3}\)\d{3}-\d{4}$/,
+  ];
   const match = validnumbers.some((number) => number.test(userInput.value));
 
   if (match) {
     results.innerText = `Valid US number: ${userInput.value}`;
     results.style.color = 'green';
     userInput.style.border = '2px solid green';
-  } else if (userInput.value == '') {
+  } else if (userInput.value === '') {
     results.innerText = '';
-    userInput.style.border = '1px solid black'
+    userInput.style.border = '1px solid black';
   } else {
     results.innerText = `Invalid US number: ${userInput.value}`;
     results.style.color = 'red';
     userInput.style.border = '2px solid red';
   }
 };
+
+checkBtn.addEventListener('click', () => {
+  if (userInput.value === '') {
+    alert('PLease provide a phone number');
+    results.innerText = '';
+  } numbervalid();
+});
 
 clearBtn.addEventListener('click', () => {
   userInput.value = '';
